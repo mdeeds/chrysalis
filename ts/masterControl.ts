3169ac3ce19d283072e0b2cc649342d820f96e21
Matt Deeds <mdeeds@gmail.com>
2020-12-24 14:16:39 -0800

    Smooth scrolling.

--- ts/masterControl.ts
+++ ts/masterControl.ts
@@ -1,4 +1,5 @@
 import { Intention } from "./intention";
+import { Shape } from "./shape";
 import { State } from "./state";
 import { World } from "./world";
 
@@ -7,40 +8,43 @@ export class MasterControl {
   startTimeMs: number;
   frameNumber: number;
   pendingEvents: Intention[];
+  private keysDown: Set<string>;
 
   constructor(world: World) {
     this.world = world;
     this.frameNumber = 0;
     this.pendingEvents = [];
+    this.keysDown = new Set<string>();
     document.addEventListener("keydown",
+      (ev) => { this.handleKey(ev); });
+    document.addEventListener("keyup",
       (ev) => { this.handleKey(ev); });
     requestAnimationFrame((ts) => { this.eventLoop(ts); });
   }
 
   handleKey(ev: KeyboardEvent) {
     if (ev.type == "keydown") {
-      switch (ev.code) {
-        case "ArrowLeft": {
-          const delta = new State();
-          delta.you.dxyz = [-0.2, 0.0, 0.0];
-          const intention = new Intention(this.frameNumber + 15,
-            delta);
-          this.pendingEvents.push(intention);
-          break;
-        }
-        case "ArrowRight": {
-          const delta = new State();
-          delta.you.dxyz = [0.2, 0.0, 0.0];
-          const intention = new Intention(this.frameNumber + 15,
-            delta);
-          this.pendingEvents.push(intention);
-          break;
-        }
-      }
+      this.keysDown.add(ev.code);
+    } else if (ev.type == "keyup") {
+      this.keysDown.delete(ev.code);
     }
   }
 
   eventLoop(ts: number) {
+    if (this.keysDown.has("ArrowLeft")) {
+      const delta = new State();
+      delta.you.dxyz = [-0.1, 0.0, 0.0];
+      const intention = new Intention(this.frameNumber + 15,
+        delta);
+      this.pendingEvents.push(intention);
+    } else if (this.keysDown.has("ArrowRight")) {
+      const delta = new State();
+      delta.you.dxyz = [0.1, 0.0, 0.0];
+      const intention = new Intention(this.frameNumber + 15,
+        delta);
+      this.pendingEvents.push(intention);
+    }
+
     const futureStack: Intention[] = [];
     for (const i of this.pendingEvents) {
       if (i.effectiveTime <= this.frameNumber) {
